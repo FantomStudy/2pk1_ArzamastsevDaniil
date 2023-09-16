f0aae4e485d4f82dc3e88e230205607cbe71d2fa
Даниил Арзамасцев <arzamastsevdaniil@gmail.com>
2023-09-16 09:32:08 +0300

    добавлена практика 3

---
+++ PZ_03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace PZ_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер дня: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+           // введем переменную дня и конвертируем ее в целочисленный формат
+            switch (day) //оператор свитч для выбора кейса
+            {
+                case int num when (num >= 1 && num <= 10): //переменная num приравнивается к переменной day. затем переменная num сравнивается со  необходимыми значениямию
+                    Console.WriteLine("принадлежит первой декаде");
+                   
+                    break;
+                case int num when (num >= 11 && num <= 20):
+                    Console.WriteLine("принадлежит второй декаде");
+                    break;
+                case int num when (num >= 21 && num <= 31):
+                    Console.WriteLine("принадлежит третьей декаде");
+                    break;
+                case int num when (num < 1 || num > 31 ): //если переменная не подходит ни под один из условий
+                    Console.WriteLine("некорректно");
+                    break;
+            }
+           
+           
+
+        }
+    }
+}
