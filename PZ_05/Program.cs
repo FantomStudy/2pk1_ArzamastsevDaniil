bef4704f232b3ed136a51fe3edca97c7b8b66cb2
Даниил Арзамасцев <arzamastsevdaniil@gmail.com>
2023-09-22 23:59:28 +0500

    добавлена 5 практика

---
+++ PZ_05/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PZ_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите целое число не меньше,чем 2: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int i = 2;
+            while (i <= a)
+            {
+                i++;
+                if (a % i == 0)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Наименьший натуральный делитель этого числа: " + i);
+        }
+    }
+}
