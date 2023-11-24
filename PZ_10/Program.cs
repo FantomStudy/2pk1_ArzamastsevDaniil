43863be324dd9d933f05687e2aa268acb6711805
kabanov.iv2182 <kabanov.iv2182@COM407-L2>
2023-11-24 10:07:05 +0300

    добавлена 10 практика

---
+++ PZ_10/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace PZ_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string[] array = str.Split(' ');
+            foreach (string text in array)
+            {
+                int ind = text.IndexOf('@');
+                if (ind != -1)
+                {
+                    string name = text.Substring(0, ind);
+                    Console.WriteLine($"hello, {name}");
+                }
+
+                
+            }
+        }
+    }
+}
