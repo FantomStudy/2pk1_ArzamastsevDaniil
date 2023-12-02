ef02db53aa041a73fc87ed3685a31d8576a19d9d
Даниил Арзамасцев <arzamastsevdaniil@gmail.com>
2023-12-02 15:12:52 +0500

    добавлена 12 практика

---
+++ PZ_12/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PZ_12
+{
+    internal class Program
+    {
+            static void Main(string[] args)
+            {
+                Console.Write("Введите строку: ");
+                string input = Console.ReadLine(); //ввод строки
+                string reversed = ReverseString(input); //вызов фунции
+
+                Console.WriteLine($"Перевернутая строка: {reversed}"); //вывод результата
+            }
+            static string ReverseString(string input) //функция, переворачивающая строку
+            {
+                char[] charArray = input.ToCharArray(); //массив символов
+
+                int left = 0; // самый левый символ
+                int right = charArray.Length - 1; // самый правый символ
+
+                while (left < right)
+                {
+                    char temp = charArray[left]; // Обмен значениями между символами
+                    charArray[left] = charArray[right];
+                    charArray[right] = temp;
+
+                    left++; // Перемещение по массиву
+                    right--;
+                }
+
+                return new string(charArray);
+            }
+        }
+    }
