f611cb06a360aa0ae7e31128d0cf6940ce5c4952
Надежда Зубкова <nad875q@gmail.com>
2023-12-08 00:36:22 +0500

    добавлена 15 практика

---
+++ PZ_15/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace PZ_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите полный путь к каталогу: ");
+                string path = Console.ReadLine(); //ввод полного пути
+                if (!Directory.Exists(path)) //если нет такого каталога, то выводится сообщение
+                {
+                    Console.WriteLine("Каталога не существует");
+                    return;
+                }
+                string[] files = Directory.GetFiles(path); //запись файлов в массив
+                foreach (string file in files) //проходимся по всем файлам в директории
+                {
+                    try
+                    {
+                        FileInfo info = new FileInfo(file); //получение информации о файле
+                        if (info.Extension == ".exe" && info.Length > 10 * 1024 * 1024) //если расширение экзэ и размер 10Мб то вывод
+                        {
+                            Console.WriteLine($"Найден файл по условию: {file}");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Ошибка при обработке файла: {file}\n{ex.Message}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+    }
+}
